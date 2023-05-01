c227deb71d8e165be1600c3ddbf0314ca3307a13
RonaldoMagalhaes <ronaldo.magalhaes1@gmail.com>
2023-05-01 19:34:58 -0300

    Add project files.

---
+++ MoviTeclas/FormMoviTeclas.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace MoviTeclas
+{
+    partial class FormMoviTeclas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMoviTeclas));
+            this.ImgLogo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ImgLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ImgLogo
+            // 
+            this.ImgLogo.Image = ((System.Drawing.Image)(resources.GetObject("ImgLogo.Image")));
+            this.ImgLogo.Location = new System.Drawing.Point(217, 222);
+            this.ImgLogo.Name = "ImgLogo";
+            this.ImgLogo.Size = new System.Drawing.Size(300, 168);
+            this.ImgLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.ImgLogo.TabIndex = 0;
+            this.ImgLogo.TabStop = false;
+            // 
+            // FormMoviTeclas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(605, 503);
+            this.Controls.Add(this.ImgLogo);
+            this.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.Name = "FormMoviTeclas";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Movimento de Imagens";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormMoviTeclas_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.ImgLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ImgLogo;
+    }
+}
+
---
+++ MoviTeclas/FormMoviTeclas.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MoviTeclas
+{
+    public partial class FormMoviTeclas : Form
+    {
+        public FormMoviTeclas()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMoviTeclas_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                if(ImgLogo.Location.X > -90)
+                {
+                    ImgLogo.Left -= 10;
+                }
+                else
+                {
+                    ImgLogo.Left = 615;
+                }
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                if (ImgLogo.Location.Y > -94)
+                {
+                    ImgLogo.Top -= 10;
+                }
+                else
+                {
+                    ImgLogo.Top = 514;
+                }
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                if (ImgLogo.Location.X < 615)
+                {
+                    ImgLogo.Left += 10;
+                }
+                else
+                {
+                    ImgLogo.Left = -99;
+                }
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                if (ImgLogo.Location.Y < 514)
+                {
+                    ImgLogo.Top += 10;
+                }
+                else
+                {
+                    ImgLogo.Top = -99;
+                }
+            }
+
+
+        }
+    }
+}
